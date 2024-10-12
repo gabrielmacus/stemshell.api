c6ca46c2815de623c296e41ee32d7aac09682ae8
Gabriel Macus <gabrielmacus@gmail.com>
2024-10-12 12:08:03 -0300

    Cambio permisos

--- src/features/user/user.controller.ts
+++ src/features/user/user.controller.ts
@@ -1,24 +1,24 @@
 import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
 import { UserService } from './user.service';
-import { Paginate, PaginateQuery } from 'nestjs-paginate';
+import { Paginate, Paginated, PaginateQuery } from 'nestjs-paginate';
 import { UserPaginationConfig } from './config/user-pagination.config';
 import { CreateUserDto, createUserSchema } from 'src/features/external/core/stemshell.shared/src/features/user/create-user.dto';
 import { ZodValidationPipe } from '../crud/zod-validation.pipe';
-import { CrudController } from '../crud/crud.controller';
 import { User } from './entities/user.entity';
 import { ReadUserDto } from 'src/features/external/core/stemshell.shared/src/features/user/read-user.dto';
 import { UpdateUserDto } from 'src/features/external/core/stemshell.shared/src/features/user/update-user.dto';
+import { CrudController } from '../crud/crud.controller';
 // USE INTERFACE
 @Controller('user')
 export class UserController extends CrudController<User,CreateUserDto,UpdateUserDto, ReadUserDto> {
   
   constructor(
     @Inject()
-    userService: UserService
+    protected userService: UserService
   ) {
-    super(userService, UserPaginationConfig)
+    super(userService, UserPaginationConfig);
   }
-
+  
   /*
   @Post()
   async create(
